8d7a8879b2b4c847c73152123335c29e9849faaf
ThomasR2004 <rietmanthomas@gmail.com>
2025-01-30 22:36:55 +0100

    Update arc.py

--- arc.py
+++ arc.py
@@ -6,7 +6,7 @@ from transformers import TextStreamer
 
 # Paths to models
 MODEL_1 = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
-MODEL_2 = "meta-llama/Llama-3.2-3B-Instruct"
+MODEL_2 = "ibm-granite/granite-3.1-8b-instruct"
 
 ADAPTER_PATH = "/gpfs/home6/trietman/ArcTTS/checkpoint-43/" # load finetune
 # Load the first model
